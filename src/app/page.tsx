73816677c18cf2f9bc2b33e3c46b763df0b5251e
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-02-26 03:11:25 -0300

    create and config styles and sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import { Header } from "@/components/Header/Header";
 import { ProfessorsSection } from "@/components/ProfessorsSection";
 import { CertificateSection } from "@/components/Certificate-section";
 import { TestimonialsSection } from "@/components/testimonials-section";
+import { PricingSection } from "@/components/pricing-section";
+import { AboutSection } from "@/components/about-section";
+import { FaqSection } from "@/components/faq-section";
+import { MiniCoursesSection } from "@/components/mini-courses-section";
+import { BibotalkSection } from "@/components/bibotalk-section";
 
 export default function Home() {
   return (
@@ -14,6 +19,11 @@ export default function Home() {
         <ProfessorsSection />
         <CertificateSection />
         <TestimonialsSection />
+        <PricingSection />
+        <AboutSection />
+        <FaqSection />
+        <MiniCoursesSection />
+        <BibotalkSection />
       </main>
       <Footer />
     </div>
